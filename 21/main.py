37e92585947fa876f1aad6071b571c522be094a3
nmyers217 <nickbmyers217@gmail.com>
2024-12-21 19:07:11 -0600

    this was really hard for some reason

--- 21/main.py
+++ 21/main.py
@@ -1,98 +1,41 @@
-import re
+from functools import cache
 
-inp = open("input.txt").read()
-
-# inp = """
-# 029A
-# 980A
-# 179A
-# 456A
-# 379A
-# """.strip()
-
+inp = open("input.txt").read().strip()
 codes = inp.splitlines()
-dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-keypad = [["7", "8", "9"], ["4", "5", "6"], ["1", "2", "3"], [None, "0", "A"]]
-dirpad = [
-    [None, "^", "A"],
-    ["<", "v", ">"],
-]
-keypad_coords = {
-    (
-        x,
-        y,
-    ): keypad[y][x]
-    for y in range(4)
-    for x in range(3)
-}
-keypad_vals = {keypad[y][x]: (x, y) for y in range(4) for x in range(3)}
-dirpad_coords = {
-    (
-        x,
-        y,
-    ): dirpad[y][x]
-    for y in range(2)
-    for x in range(3)
-}
-dirpad_vals = {dirpad[y][x]: (x, y) for y in range(2) for x in range(3)}
+keypad = ["789", "456", "123", " 0A"]
+dirpad = [" ^A", "<v>"]
 
 
-def calculate_all_sequences(code, keypad_coords, keypad_vals):
-    def recurse(code, seq, cur):
-        if not code:
-            return [seq]
+def dfs(pad, cur, dest):
+    x, y = next((x, y) for y, r in enumerate(pad) for x, c in enumerate(r) if c == cur)
+    x2, y2 = next(
+        (x, y) for y, r in enumerate(pad) for x, c in enumerate(r) if c == dest
+    )
 
-        dest = keypad_vals[code[0]]
+    def g(x, y, seq):
+        if (x, y) == (x2, y2):
+            yield seq + "A"
+        if x2 < x and pad[y][x - 1] != " ":
+            yield from g(x - 1, y, seq + "<")
+        if y2 < y and pad[y - 1][x] != " ":
+            yield from g(x, y - 1, seq + "^")
+        if y2 > y and pad[y + 1][x] != " ":
+            yield from g(x, y + 1, seq + "v")
+        if x2 > x and pad[y][x + 1] != " ":
+            yield from g(x + 1, y, seq + ">")
 
-        if cur == dest:
-            return recurse(code[1:], seq + "A", cur)
-
-        sequences = []
-
-        x, y = cur
-        x2, y2 = dest
-        dx, dy = x2 - x, y2 - y
-
-        if dx > 0 and keypad_coords.get((x + 1, y)):
-            sequences.extend(recurse(code, seq + ">", (x + 1, y)))
-        if dy < 0 and keypad_coords.get((x, y - 1)):
-            sequences.extend(recurse(code, seq + "^", (x, y - 1)))
-        if dy > 0 and keypad_coords.get((x, y + 1)):
-            sequences.extend(recurse(code, seq + "v", (x, y + 1)))
-        if dx < 0 and keypad_coords.get((x - 1, y)):
-            sequences.extend(recurse(code, seq + "<", (x - 1, y)))
-
-        return sequences
-
-    return recurse(code, "", keypad_vals["A"])
+    # Optimize for paths that have the least abount of zig-zagging between directions
+    return min(g(x, y, ""), key=lambda pad: sum(a != b for a, b in zip(pad, pad[1:])))
 
 
-def calculate_shortest_sequence(key_code):
-    sequences = calculate_all_sequences(key_code, keypad_coords, keypad_vals)
-    sequences = [
-        item
-        for sublist in [
-            calculate_all_sequences(seq, dirpad_coords, dirpad_vals)
-            for seq in sequences
-        ]
-        for item in sublist
-    ]
-    sequences = [
-        item
-        for sublist in [
-            calculate_all_sequences(seq, dirpad_coords, dirpad_vals)
-            for seq in sequences
-        ]
-        for item in sublist
-    ]
-    return min(sequences, key=len)
+@cache
+def dp(code, robot, chain_length):
+    if robot > chain_length:
+        return len(code)
+    pad = keypad if robot == 0 else dirpad
+    pairs = zip("A" + code, code)
+    return sum(dp(dfs(pad, cur, dest), robot + 1, chain_length) for cur, dest in pairs)
 
 
-def score(code):
-    seq = calculate_shortest_sequence(code)
-    prefix = int(re.match(r"(.*)A", code).group(1))
-    print(f"{len(seq)} * {prefix} = {prefix * len(seq)}")
-    return len(seq) * prefix
-
-
-print(sum(score(code) for code in codes))
+print(sum(dp(code, 0, 2) * int(code[:-1]) for code in codes))
+print(sum(dp(code, 0, 25) * int(code[:-1]) for code in codes))
